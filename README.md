a3fee6c4efef7afb951162e14f54f435bfbe51e7
Siavash <87589345+chaharlangi@users.noreply.github.com>
2024-03-23 11:17:39 +0100

    FirstPush

---
+++ README.md
@@ -0,0 +1,2 @@
+# SecureDigitalHealthcare
+ 
---
+++ SecureDigitalHealthcare/Models/SecureDigitalHealthcareContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecureDigitalHealthcare.Models;
+
+public partial class SecureDigitalHealthcareContext : DbContext
+{
+    public SecureDigitalHealthcareContext()
+    {
+    }
+
+    public SecureDigitalHealthcareContext(DbContextOptions<SecureDigitalHealthcareContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.ToTable("Student");
+
+            entity.Property(e => e.LastName)
+                .HasMaxLength(10)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
