8442377c61e6185bd052f27d089e47bd68c16076
chaharlangi <ali4langi1899@gmail.com>
2024-04-30 17:26:39 +0200

    Authorization part 1

---
+++ SecureDigitalHealthcare/Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureDigitalHealthcare.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public record AuthenticationData(string? UserName, string? Password);
+        public record UserData(string UserId, string UserName);
+
+        [HttpPost("token")]
+        public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+        {
+
+            return Unauthorized();
+        }
+
+        private UserData
+
+        private bool CompareValues(string? actual, string expected)
+        {
+            if (actual is not null)
+            {
+                if (actual.Equals(expected))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
